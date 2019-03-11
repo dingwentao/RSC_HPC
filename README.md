2ccbe44eabae536805d488ec69298cd3275165b3
dingwentao <ustc.dingwentao@gmail.com>
2019-03-11 13:14:07 -0500

    update createjobs.sh

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RSC_HPC
+
+Scripts for parallel processing of ADR and Matlab code
--- createjobs.sh
+++ createjobs.sh
@@ -1,10 +1,10 @@
-#DIR="/data/Japan_Project_data/20181220"
 DIR=$1
 hostname=$(hostname)
 nodeid=$(echo $hostname | cut -c8-9)
 jobstxt="jobs$nodeid.txt"
 
-touch ./$jobstxt
+cp /dev/null $jobstxt
+
 for i in $(ls $DIR| grep dat); do
 	echo $DIR/$i >> ./$jobstxt;
 done
